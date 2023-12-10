826597d9fac67cb72b18b0936c770c48d028eb50
Jackbinson <150789230+Jackbinson@users.noreply.github.com>
2023-12-10 16:13:21 +0700

    Create .js

---
+++ .js
@@ -0,0 +1,15 @@
+alert("Welcome to javascript testing website")
+let user = prompt("Tên của bạn là gì ?")
+console.log(user);
+alert("Chào mừng " + user)
+let gender = prompt("Bạn là nam/nữ ?")
+gender == 'nam' 
+console.log(gender);
+if (gender == 'nam') {
+    alert("Giới tính của bạn là nam");
+    console.log("Tôi là nam");
+}
+else {
+    alert("Giới của bạn là nữ");
+    console.log("Tôi là nữ");
+}
