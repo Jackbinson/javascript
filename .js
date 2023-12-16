0a407d49f79cc6eeb97d72d6681b88e267d07825
Jackbinson <150789230+Jackbinson@users.noreply.github.com>
2023-12-16 19:22:58 +0700

    Update .js

--- .js
+++ .js
@@ -3,6 +3,8 @@ alert("Welcome to javascript testing website")
 let user = prompt("Tên của bạn là gì ?")
 console.log(user);
 alert("Chào mừng " + user)
+let color = prompt("Enter Color Name");
+document.body.style.background = color;
 let gender = prompt("Bạn là nam/nữ ?")
 gender == 'nam' 
 console.log(gender);
